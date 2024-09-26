c5e3bee3e3ad879c732bafa132f8a4aa9687b038
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-09-26 20:41:53 +0200

    🚧 Initialisation : Create layout with Header and Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashboardPage from "./components/pages/dashboard/DashboardPage";
+import Layout from "./components/reusable-ui/Layout";
 
 export default function App() {
   return (
+    <div>
+      <Layout/>
       <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<DashboardPage/>} />
-      </Routes>
+        <Routes>
+          <Route path={"/"} element={<DashboardPage />} />
+        </Routes>
       </BrowserRouter>
-      
- 
+    </div>
   );
 }
