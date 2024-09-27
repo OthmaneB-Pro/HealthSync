5414e864f386f7d425c6b72fa2d112b38dc81306
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-09-27 10:43:27 +0200

    ✨ Initialisation : add element to header

--- src/components/reusable-ui/Header.tsx
+++ src/components/reusable-ui/Header.tsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <HeaderStyled>
+      <div className="title">
+        <img src="img/HealthSyncLogo.PNG" alt="LogoHealth" />
+        <h2>HealthSync</h2>
+      </div>
+      <div className="user">
+        <h3>Othmane</h3>
+        <img src="" alt="Z" />
+      </div>
+    </HeaderStyled>
+  );
 }
+
+const HeaderStyled = styled.div`
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  background-color: green;
+
+  .title {
+    display: flex;
+    background-color: purple;
+    img{
+      width: 80px;
+      height: auto;
+    }
+  }
+  .user{
+    display: flex;
+
+    img{
+        width: 50px;
+        height: 50px;
+        background-color: blue;
+    }
+  }
+`;
