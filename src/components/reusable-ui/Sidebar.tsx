fb8f4d705c987b567bd3aed3c02da88622512cb0
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-09-27 11:42:31 +0200

    ✨ Initialisation : Sidebar > Add all elements

--- src/components/reusable-ui/Sidebar.tsx
+++ src/components/reusable-ui/Sidebar.tsx
@@ -1,5 +1,20 @@
+import { HiHome } from "react-icons/hi";
+import { FaPencilAlt, FaRunning } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
+import { TbLogout } from "react-icons/tb";
+
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div>
+      <ul>
+        <li><a href="/"><HiHome />Dashboard</a></li>
+        <li><a href="/"><FaPencilAlt />Suivi des repas</a></li>
+        <li><a href="/"><FaRunning />Activité physique</a></li>
+        <li><a href="/"><IoSettingsSharp />Paramètres</a></li>
+      </ul>
+      <div>
+        <button><TbLogout />Se déconnecter</button>
+      </div>
+    </div>
+  );
 }
