8240268782b5027f4643868ebda1440fee801252
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-10-25 15:57:11 +0200

    ✨ Activity : route to activity

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Layout from "./components/layout/Layout";
 import "./App.css";
 import ErrorPage from "./components/pages/error/ErrorPage";
 import MealTrackingPage from "./components/pages/mealTracking/MealTrackingPage";
+import ActivityPage from "./components/pages/activity/ActivityPage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
             <Route path={"*"} element={<ErrorPage />} />
             <Route index element={<DashboardPage />} />
             <Route path={"/mealTracking"} element={<MealTrackingPage />} />
+            <Route path={"/activity"} element={<ActivityPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
