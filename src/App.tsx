73a993daf103f526dc71113f4f404a38e2de8890
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-10-26 14:53:00 +0200

    ✨ Parameter : add route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 import ErrorPage from "./components/pages/error/ErrorPage";
 import MealTrackingPage from "./components/pages/mealTracking/MealTrackingPage";
 import ActivityPage from "./components/pages/activity/ActivityPage";
+import ParameterPage from "./components/pages/settingsPage/ParameterPage";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
             <Route index element={<DashboardPage />} />
             <Route path={"/mealTracking"} element={<MealTrackingPage />} />
             <Route path={"/activity"} element={<ActivityPage />} />
+            <Route path={"/parameters"} element={<ParameterPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/pages/settingsPage/ParameterPage.tsx
@@ -0,0 +1,9 @@
+import Title from "../../reusable-ui/Title";
+
+export default function ParameterPage() {
+  return (
+    <div>
+        <Title label="Paramètres"/>
+    </div>
+  )
+}
